61302359ca7397ac5c6e95adf77efa71ea61fb2f
PdLoaderR <parsa.bahrami@gmail.com>
2020-08-14 00:51:31 -0400

    adding trigger to delete config files before destroy

--- RAC/OL7D122/shared/files/passwordlesssssh_finalize.sh
+++ RAC/OL7D122/shared/files/passwordlesssssh_finalize.sh
@@ -1,3 +1,3 @@
 ## This file is auto generated on each run
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no user@OL7D122RAC01 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no user@OL7D122RAC02 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
+sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC01 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
+sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC02 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
