859b417dae2068c7c45fbfd82a12d6efab178b35
PdLoaderR <parsa.bahrami@gmail.com>
2020-08-15 06:39:14 -0400

    add ssh-keyscan step in setting up passwordless ssh

--- RAC/OL7D122/shared/scripts/shared_config_passwordless_ssh_finalize.sh
+++ RAC/OL7D122/shared/scripts/shared_config_passwordless_ssh_finalize.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cat /vagrant/shared/files/authorized_keys_root > /root/.ssh/authorized_keys
+sh /vagrant/shared/files/passwordlesssssh_ssh-keyscan.sh
 su - oracle -c 'cat /vagrant/shared/files/authorized_keys_oracle > /home/oracle/.ssh/authorized_keys'
+su - oracle -c 'sh /vagrant/shared/files/passwordlesssssh_ssh-keyscan.sh'
