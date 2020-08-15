dbb357786ef387723222aac3dded3ff636a5bb32
PdLoaderR <parsa.bahrami@gmail.com>
2020-08-15 10:36:36 -0400

    removing ssh-keyscan

--- RAC/OL7D122/shared/files/passwordlesssssh_finalize.sh
+++
@@ -1,5 +0,0 @@
-## This file is auto generated on each run
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC01 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC02 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC03 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
-sshpass -f /tmp/temp1.txt ssh -o StrictHostKeyChecking=no root@OL7D122RAC04 'sh /vagrant/shared/scripts/shared_config_passwordless_ssh_finalize.sh' 
--- RAC/OL7D122/shared/files/passwordlesssssh_ssh-keyscan.sh
+++
@@ -1,5 +0,0 @@
-## This file is auto generated on each run
-ssh-keyscan -H OL7D122RAC01 >> ~/.ssh/known_hosts 
-ssh-keyscan -H OL7D122RAC02 >> ~/.ssh/known_hosts 
-ssh-keyscan -H OL7D122RAC03 >> ~/.ssh/known_hosts 
-ssh-keyscan -H OL7D122RAC04 >> ~/.ssh/known_hosts 
